74fb3c98a40e7d916e6cc1451f371a4272eafd54
Mark Slorach <hello@markslorach.com>
2024-04-30 13:16:20 +0100

    set up clerk auth

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,10 @@
+import { UserButton } from "@clerk/nextjs"
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div>Dashboard
+      <UserButton />
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
 export default function Home() {
+
+  const { userId } = auth();
+
+  if (userId) {
+    redirect('/dashboard')
+  }
+
   return (
     <main className="max-w-md mx-auto flex justify-center items-center min-h-screen">
-      <h1 className="font-extrabold text-5xl">CodeCrush.</h1>
+      <div className="flex flex-col space-y-5">
+        <h1 className="font-extrabold text-5xl">CodeCrush.</h1>
+        <SignedIn>
+          <Button>
+            <Link href="/dashboard">Go to dashboard</Link>
+          </Button>
+        </SignedIn>
+        <SignedOut>
+          <SignInButton>
+            <Button className="border">Sign In</Button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </main>
   );
 }
