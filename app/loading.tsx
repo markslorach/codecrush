13c710d0f5c2b0cecf0051c0f2c32086a233a8e2
Mark Slorach <markslorach@Marks-MacBook-Pro.local>
2024-07-30 19:38:46 +0100

    loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import { FaCode } from "react-icons/fa";
 
 const loading = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
+    <div className="flex h-dvh w-full items-center justify-center">
       <FaCode className="h-10 w-10 animate-spin text-white/70" />
     </div>
   );
