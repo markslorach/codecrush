c77fa3dd484d2cb61d95f49d1353e9f9304d69e1
Mark Slorach <markslorach@Marks-MacBook-Pro.local>
2024-07-31 09:16:07 +0100

    sidenav in loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,15 @@
 import { FaCode } from "react-icons/fa";
+import SideNav from "./(app)/components/SideNav";
 
 const loading = () => {
   return (
-    <div className="flex h-dvh w-full items-center justify-center">
-      <FaCode className="h-10 w-10 animate-spin text-white/70" />
+    <div className="grid grid-cols-1 md:grid-cols-[80px_1fr]">
+      <div className="hidden min-h-screen flex-col border-r border-white/10 py-6 md:flex">
+        <SideNav />
+      </div>
+      <div className="flex h-dvh w-full items-center justify-center">
+        <FaCode className="h-10 w-10 animate-spin text-white/70" />
+      </div>
     </div>
   );
 };
